3439d5be8716ab118fdad3b334d3983e3447dd59
Tovarm <tovamosk@gmail.com>
2014-12-17 16:20:47 -0500

    adding some lines

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,10 +1,13 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "Tova",#changethis
-  :database => "anagram"
-  })
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] +  '@127.0.0.1/anagram')
+
+
+
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "Tova",#changethis
+#   :database => "anagram"
+#   })
 
   ActiveRecord::Base.logger = Logger.new(STDOUT)  
