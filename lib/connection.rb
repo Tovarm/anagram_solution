c999f9793eb6a5720cf9b90f818637dbb3338006
Tovarm <tovamosk@gmail.com>
2014-12-17 14:47:01 -0500

    added bundler and gemfile

--- lib/connection.rb
+++ lib/connection.rb
@@ -3,8 +3,8 @@ require 'active_record'
 ActiveRecord::Base.establish_connection({
   :adapter => "postgresql",
   :host => "localhost",
-  :username => "andrewfritz",#changethis
-  :database => "wordplay"
+  :username => "Tova",#changethis
+  :database => "anagram"
   })
 
   ActiveRecord::Base.logger = Logger.new(STDOUT)  
